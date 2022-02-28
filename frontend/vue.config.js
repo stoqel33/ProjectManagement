15fcacde2cee984ed638f096821c0bb25a329aee
stoqel33 <stochel202@interia.pl>
2022-02-28 22:13:12 +0100

    add and setup new package - i18n for translation

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,11 @@
-const { defineConfig } = require('@vue/cli-service');
-
-module.exports = defineConfig({
+module.exports = {
   transpileDependencies: true,
-});
+  chainWebpack: (config) => {
+    config.module
+      .rule('i18n')
+      .resourceQuery(/blockType=i18n/)
+      .type('javascript/auto')
+      .use('i18n')
+      .loader('@intlify/vue-i18n-loader')
+  },
+}
